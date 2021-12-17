11f6b886445d36f83ef9166c1d16f4df782c89fb
yim58 <yim58@DESKTOP-0UCDELL>
2021-12-17 11:09:28 +0900

    setter/getter 잘못되서 지움 -master-

--- gitTest/src/com/git/model/vo/Member.java
+++ gitTest/src/com/git/model/vo/Member.java
@@ -6,41 +6,5 @@ public class Member {
 	private int age;
 	private char gender;
 	
-	public Member() {
-	
-	}
-	
-	public Member(String name, int age, char gender) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public char getGender() {
-		return gender;
-	}
-
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
-	
-	
 	
 }
